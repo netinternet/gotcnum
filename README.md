e64e926e7536ff8bd08eb19adf65a7d41a68435f
Osman Makal <osman.makal@ni.net.tr>
2017-11-01 11:05:41 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # go-tcnum
 Turkish Identification Number Validation
+
+## Usage
+
+```
+go get github.com/netinternet/go-tcnum
+```
+
+## Exemple
+
+```go
+package main
+
+import (
+    "fmt"
+    "github.com/netinternet/go-tcnum"
+)
+
+func main() {
+    if valid := VerifyTcnum("10148343778"); valid {
+      fmt.Println("TC Number Is Valid")
+    } else {
+      fmt.Println("TC Number Is Not Valid")
+    }
+}
+
+```
---
+++ tcnum.go
@@ -0,0 +1,34 @@
+package tcnum
+
+import "strconv"
+
+func tcnum(n string) bool {
+	var even, odd, total, digits, digit10, digit11 int
+	if len(n) != 11 {
+		return false
+	}
+	for i := 0; i < 11; i++ {
+		str2int, _ := strconv.Atoi(string(n[i]))
+		digits++
+		if digits <= 9 {
+			if digits%2 == 0 {
+				even = even + str2int
+			} else {
+				odd = odd + str2int
+			}
+			total = total + str2int
+		} else if digits == 10 {
+			total = total + str2int
+			digit10 = ((odd * 7) - even) % 10
+			if digit10 != str2int {
+				return false
+			}
+		} else if digits == 11 {
+			digit11 = total % 10
+			if digit11 != str2int {
+				return false
+			}
+		}
+	}
+	return true
+}
