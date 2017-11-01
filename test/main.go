b608e2a23d8563d3964066deca5bee60c5257d7e
Osman Makal <osman.makal@ni.net.tr>
2017-11-01 11:14:03 +0300

    test

---
+++ test/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+ 	"github.com/netinternet/gotcnum"
+)
+
+func main() {
+	if valid := gotcnum("10148343778"); valid {
+		fmt.Println("TC Number Is Valid")
+	} else {
+		fmt.Println("TC Number Is Not Valid")
+	}
+}
